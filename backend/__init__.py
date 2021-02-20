83f128a218aef546649fb1917546f9b992412dcc
momolin78 <momolin78@gmail.com>
2021-02-19 19:26:04 -0500

    linking backend to database

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from flask_mysqldb import MySQL
+import os
+
+
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASS') 
 
 def create_app(test_config=None):
     app = Flask(__name__)
     cors = CORS(app)
+
+    print("database")
+    #database configurations
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = db_user
+    app.config['MYSQL_PASSWORD'] = db_password
+    app.config['MYSQL_DB'] = 'cadreDB'
+    
+    mysql = MySQL(app)
     
     #app configurations
     app.config["JSON_SORT_KEYS"] = False
@@ -14,5 +29,13 @@ def create_app(test_config=None):
     @cross_origin()
     def mainPage():
         return {'greeting': "Hello World"}
+    
 
+    @app.route('/students')
+    def students():
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM Students")
+        rv = cur.fetchall()
+        return str(rv)
+    
     return app
