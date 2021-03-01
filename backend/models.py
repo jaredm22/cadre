3ac51065a584e12ba81cea7ca97b12e0a5e4417c
momolin78 <momolin78@gmail.com>
2021-03-01 13:06:25 -0500

    adding comments

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from backend import db
 
 class Student(db.Model):
+	# Database model for student table. 
 	__tablename__ = 'student'
 	id = db.Column(db.Integer(), primary_key=True)
 	firstName = db.Column(db.String())
@@ -9,6 +10,7 @@ class Student(db.Model):
 	course = db.Column(db.String())
 
 class Professor(db.Model):
+	# Database model for Professor table. 
 	__tablename__ = "Professor"
 	id = db.Column(db.Integer(), primary_key=True)
 	professorId = db.Column(db.Integer())
@@ -17,6 +19,7 @@ class Professor(db.Model):
 	email = db.Column(db.String())
 
 class Course(db.Model):
+	# Database model for Course table. 
 	__tablename__ = "Course"
 	id = db.Column(db.Integer(), primary_key=True)
 	courseId = db.Column(db.Integer())
--- backend/views.py
+++ backend/views.py
@@ -3,23 +3,30 @@ from backend import app, db
 from backend.models import *
 
 @app.route("/api")
+# Main route of the app.
 def index():
     return {'greeting': "Hello World"}
 
 @app.route("/students")
+# Route to show all students in the app database.
 def show_students():
+    # Querying all students in the database.
     students = Student.query.all()
     student_list = []
     for student in students:
+        # Listing the id, firstname, lastname, email, and course of each student.
         stud = {'id': student.id, 'firstname': student.firstName, 'lastname': student.lastName, 'email': student.email}
         student_list.append(stud)
     return jsonify(students = student_list)
 
 @app.route("/studentCourses")
+# Route to list all courses that a student is taking. 
 def get_student_courses():
+    # Parsing inputted json.
     content =  request.get_json()
+    # Getting back the email of the student
     email = content["email"]
-
+    # Querying list of courses by student email.
     courses = Student.query.filter_by(email=email).all()
     results = [
         {
@@ -30,28 +37,38 @@ def get_student_courses():
 
 
 @app.route("/studentInfo")
+# Route to show a specified student info.
 def get_student_info():
+    # Parsing inputted json.
     content =  request.get_json()
+    # Getting email of the student
     email = content["email"]
-
+    # Querying the student.
     info = Student.query.filter_by(email= email).first()
+    # Returning firstname, lastname, and email of the student.
     return jsonify(firstName = info.firstName, lastName = info.lastName, email= info.email)
 
 @app.route("/professors")
+# Route to show all students in the app database.
 def show_professors():
     professors = Professor.query.all()
+    # Querying all professor in app database.
     professor_list = []
     for professor in professors:
+        # Listing the id, firstname, lastname, email, and course of each professor.
         prof = {'id': professor.id, 'firstname': professor.firstName, 'lastname': professor.lastName, 'professorId': professor.professorId, 'email': professor.email}
         professor_list.append(prof)
     return jsonify(professors = professor_list)
 
 
 @app.route("/courses")
+# Route to show all courses in the app database.
 def show_courses():
+    # Querying all courses
     courses = Course.query.all()
     course_list = []
     for course in courses:
+        # Listing the id, course name, zoom link, and course id of each course.
         cour= {'id': course.id, 'coursename': course.courseName, 'zoomLink': course.zoomLink, 'courseId': course.courseId}
         course_list.append(cour)
     return jsonify(courses = course_list)
