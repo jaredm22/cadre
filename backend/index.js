518f8d821646b1df66f7ad8e8a1d8b2ccac6b353
momolin78 <momolin78@gmail.com>
2021-03-14 18:18:49 -0400

    fixing frontend integration

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 //  app.use(express.static("public"));
 
-app.use((req, res, next) => {
+app.get("/", async (req, res, next) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
