e1fbd9fc2d7c2e2d68a1ae45f692582c209004cc
Jared Min <jkymin2@gmail.com>
2021-03-14 15:53:50 -0400

    Fixed merge conflict

--- frontend/src/apiHandle.js
+++ frontend/src/apiHandle.js
@@ -7,13 +7,9 @@ var config = {
   },
 };
 
-<<<<<<< HEAD
+
 const getStudents = () => {
     return axios.get("/students")
-=======
-const getHello = () => {
-  return axios.get("/students");
->>>>>>> b9c17ba1c9a92823cb8e270c6e457ce9d177e1f4
 };
 
 const getCourses = () => {
@@ -43,17 +39,8 @@ const getLectureDates = (name) => {
 };
 
 export default {
-<<<<<<< HEAD
     getStudents,
     getStudentCourses,
     getCourses,
     getLectureDates
-
 }
-=======
-  getHello,
-  getStudentCourses,
-  getCourses,
-  getLectureDates,
-};
->>>>>>> b9c17ba1c9a92823cb8e270c6e457ce9d177e1f4
