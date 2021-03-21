5e4690ce34c6e20d9209d95c4bc39d0024b94e3f
Jared Min <jkymin2@gmail.com>
2021-03-21 19:22:12 -0400

    Fixed dependency issue

--- frontend/src/pages/login/LoginForm.js
+++ frontend/src/pages/login/LoginForm.js
@@ -3,19 +3,22 @@ import {
   Form,
   FormGroup,
   TextInput,
-  ActionGroup,
   Button,
-} from "@patternfly/react-core";
+  Grid,
+  Row,
+  Column,
+} from "carbon-components-react/";
 import api from "../../apiHandle";
-import "@patternfly/react-styles/css/components/Form/form.css";
 import "./form.css";
 
 export default class LoginForm extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       email: "",
     };
+
     this.handleEmailChange = (email) => {
       this.setState({ email });
     };
@@ -30,48 +33,30 @@ export default class LoginForm extends React.Component {
     const { email } = this.state;
 
     return (
-      <div className="form">
-        <Form isHorizontal onSubmit={this.submitHandler}>
-          <h2>Login</h2>
+      <Grid>
+        <Form onSubmit={this.submitHandler}>
+          <Row>
+            <h2>View your Schedule</h2>
+          </Row>
 
-          <FormGroup
-            label="Email"
-            isRequired
-            fieldId="horizontal-form-name"
-            helperText="Please enter your email (For privacy during testing, please enter email as FirstInitialLastName@bu.edu (eg. John Smith - jsmith@bu.edu)"
-          >
-            <TextInput
-              value={email}
-              isRequired
-              type="text"
-              id="horizontal-form-name"
-              aria-describedby="horizontal-form-name-helper"
-              name="horizontal-form-name"
-              onChange={this.handleEmailChange}
-            />
-          </FormGroup>
-
-          {/* <FormGroup label="School" fieldId="horizontal-form-title">
-                    <FormSelect
-                        value={this.state.school}
-                        onChange={this.onSchoolChange}
-                        id="horzontal-form-title"
-                        name="horizontal-form-title"
-                        aria-label="School"
-                    >
-                        {this.schoolOptions.map((option, index) => (
-                        <FormSelectOption isDisabled={option.disabled} key={index} value={option.value} label={option.label} />
-                        ))}
-                    </FormSelect>
-                </FormGroup> */}
-
-          <ActionGroup>
-            <Button variant="primary" type="submit">
-              Submit form
-            </Button>
-          </ActionGroup>
+          <Row>
+            <Column>
+              <FormGroup className="email-input" legendText="Email">
+                <TextInput
+                  id="email"
+                  value={email}
+                  placeholderText="Email"
+                  onChange={this.handleEmailChange}
+                />
+              </FormGroup>
+            </Column>
+          </Row>
         </Form>
-      </div>
+
+        <Button variant="primary" type="submit" className="submit">
+          Submit
+        </Button>
+      </Grid>
     );
   }
 }
